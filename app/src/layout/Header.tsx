15ed5fcd49effa7c0bd54b57aeeaa8053a3a1214
Onkar Patil <102043839+Foxhog-x@users.noreply.github.com>
2024-06-18 16:02:03 +0530

    created profile page

--- app/src/layout/Header.tsx
+++ app/src/layout/Header.tsx
@@ -1,5 +1,13 @@
+import { useState } from "react";
 import styles from "./Header.module.css";
+
 export const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <>
       <header>
@@ -7,13 +15,18 @@ export const Header = () => {
           <div className={styles.logo}>
             <span style={{ marginLeft: "44px" }}>acmeCo</span>
           </div>
-          <div className={styles.navlinks}>
+          <div className={`${styles.navlinks} ${isOpen ? styles.active : ""}`}>
             <li>Home</li>
             <li>Books</li>
             <li>Guest</li>
             <li>Events</li>
             <li>Services</li>
             <span>Logo</span>
+          </div>
+          <div className={styles.hamburger} onClick={toggleMenu}>
+            <div></div>
+            <div></div>
+            <div></div>
           </div>
         </div>
       </header>
--- app/src/pages/profilepage/Profilepage.tsx
+++ app/src/pages/profilepage/Profilepage.tsx
@@ -1,3 +1,79 @@
+import styles from "./Profile.module.css";
+import PhotoCameraIcon from "@mui/icons-material/PhotoCamera";
 export const Profilepage = () => {
-  return <div>Profilepage</div>;
+  return (
+    <>
+      <div className={styles.profile_container}>
+        <div className={styles.profile_heading}>
+          <h2>My Profile</h2>
+        </div>
+        <div style={{ marginBottom: 35 }}>
+          <button
+            style={{
+              padding: "1rem",
+              maxWidth: 275,
+              minWidth: 100,
+              borderRadius: "1rem",
+            }}
+          >
+            <span>logo</span> Add Profile Photo here
+          </button>
+        </div>
+
+        <form
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            gap: 35,
+            padding: 5,
+          }}
+        >
+          <div className={styles.fields}>
+            <div className={styles.label_filed}>
+              <label htmlFor="">FirstName</label>
+              <input type="text" name="firstName" />
+            </div>
+            <div className={styles.label_filed}>
+              <label htmlFor="">LastName</label>
+              <input type="text" name="lastName" />
+            </div>
+          </div>
+          <div className={styles.label_email}>
+            <label>Email</label>
+            <input type="text" name="email" />
+          </div>
+          <div className={styles.address}>
+            <label>Address</label>
+            <textarea
+              style={{ maxWidth: 510 }}
+              name="address"
+              rows={7}
+            ></textarea>
+          </div>
+          <div className={styles.submit_container}>
+            <button
+              style={{
+                padding: 12,
+                minWidth: 100,
+                borderRadius: "1rem",
+              }}
+            >
+              Reset
+            </button>
+            <button
+              style={{
+                padding: 12,
+                minWidth: 100,
+                borderRadius: "1rem",
+                backgroundColor: "#535bf2",
+              }}
+              type="submit"
+            >
+              Submit
+            </button>
+          </div>
+        </form>
+      </div>
+    </>
+  );
 };
