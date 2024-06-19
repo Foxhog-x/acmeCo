50d0d1c9433587bb48acbde5c2cea91fe7ed2fca
Onkar Patil <102043839+Foxhog-x@users.noreply.github.com>
2024-06-19 17:05:17 +0530

    some ui changes

--- app/src/layout/Header.tsx
+++ app/src/layout/Header.tsx
@@ -27,7 +27,15 @@ export const Header = () => {
               src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png"
             />
           </div>
-          <div className={styles.hamburger} onClick={toggleMenu}></div>
+          <div
+            style={{ marginRight: 15 }}
+            className={styles.hamburger}
+            onClick={toggleMenu}
+          >
+            <div></div>
+            <div></div>
+            <div></div>
+          </div>
         </div>
       </header>
     </>
